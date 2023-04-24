25f807ce82ac320c870c48ef0de33f600288b740
Flavio Coradini <flaviocoradini@Flavios-MacBook-Pro.local>
2023-04-24 17:43:26 +0900

    second

--- src/pages/components/NavBar.tsx
+++ src/pages/components/NavBar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function NavBar() {
   return (
     <div className="flex justify-between p-10 font-bold bg-gray-300">
-      <h1 className="text-2xl">My DAPP</h1>
+      <h1 className="text-2xl">DAPP</h1>
       <h1 className="text-2xl">Profile</h1>
     </div>
   );
